5ab53a96ab29760050c37226dbe74d3859601286
Wiliam <william_ho_swee_chiong@hotmail.com>
2021-10-11 16:53:26 +0800

    Added new Gamemap Anor Londo and Fog Doorrr

---
+++ src/game/FogDoor.java
@@ -0,0 +1,33 @@
+package game;
+
+import edu.monash.fit2099.engine.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FogDoor extends Ground {
+    World world;
+    GameMap secMap;
+
+    public FogDoor(){
+        super('=');
+    }
+    public FogDoor(GameMap secMap) {
+        super('=');
+        this.secMap=secMap;
+    }
+
+    @Override
+    public Actions allowableActions(Actor actor, Location location, String direction) {
+        Location location1 = secMap.at(20,0);
+        Actions actions=new Actions();
+        actions.add(new MoveActorAction(location1,direction,"z"));
+        return actions;
+    }
+
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return true;
+    }
+}
